13b4a87b9688ab6fd0c28cf2c3968cf182306149
Breno Matos <brenodematos2002@gmail.com>
2023-01-10 13:01:34 -0300

    Implementando novas funções VUE

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,44 @@
 <template>
-  <Navbar :logo="logo_src" :alt="app_name"/>
-  <router-view/>
-  <Footer />
+  <div>
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <div>
+      <router-view></router-view>
+    </div>
+    <Footer />
+  </div>
 </template>
-
-  <script>
-  import Navbar from './components/Navbar.vue';
-  import Footer from './components/Footer.vue';
-
-
-    export default {
-      components: {
-        Navbar,
-        Footer
-      },
-      data() {
-        return {
-          logo_src: "/img/logo.png",
-          app_name: "Make Your Burger"
-        }
-      }
-
+<script>
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+export default {
+  data: function() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger"
     }
-
+  },
+  components: {
+    Navbar,
+    Footer
+  }
+}
 </script>
+
 <style>
-
-* {
-  font-family: helvetica;
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-
-.main-container {
-  margin: 50px;
-  min-height: 250px;
-}
- 
-h1 {
-  text-align: center;
-  font-size:  42px;
-  margin-bottom: 30px;
-  color: #222
-}
- 
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
